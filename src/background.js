aa0177992aa8222bb94f79d65970e74b9c449566
Piotr Grela <piotrg5533@gmail.com>
2022-06-08 11:19:23 +0200

    static hashing on the extracted data almost works

--- src/background.js
+++ src/background.js
@@ -1,51 +1,64 @@
 'use strict';
 
-import * as crypto from "crypto-js"
+import * as cryptoJs from "crypto-js";
 
-const hash = crypto.algo.SHA256.create();
+// Keep data extracted from tabs by content script
+let tabsData = [];
+// Keep data about ongoing downloads
+let downloads = {};
 
 /**
  * Greet the user upon install/update
  */
 chrome.runtime.onInstalled.addListener(() => {
     chrome.tabs.create({'url': 'welcome-page.html'});
-    console.debug("Inside welcome page");
 });
-
-/**
- * Collect data about ongoing downloads.
- * @type {{}}
- */
-let downloads = {};
 
 /**
  * Listen for a started download.
  */
 chrome.downloads.onCreated.addListener((downloadItem) => {
-    console.log("Download started!");
+    console.debug("Download started!");
+    for (let tab of tabsData) {
+        if (tab.urls.includes(downloadItem.url) || tab.urls.includes(downloadItem.finalUrl)) {
+            downloads[downloadItem.id] = {
+                tab: tab,
+                url: downloadItem.url,
+                checksums: tab.checksums,
+                algorithms: tab.algorithms
+            }
+            keepAlive();
+            break;
+        }
+    }
 });
 
 /**
  * Listen for a change in download.
  */
-chrome.downloads.onChanged.addListener((downloadDelta) => {
+chrome.downloads.onChanged.addListener(async (downloadDelta) => {
     console.debug("Download changed!");
     console.debug(downloadDelta);
-    // testFetch();
     if (downloadDelta.filename) {
         const filename = downloadDelta.filename.current;
         if (filename !== "") {
-            downloads[downloadDelta.id] = {
-                filename: "file://" + filename
-            };
+            downloads[downloadDelta.id].filename = "file://" + filename;
         }
     } else if (downloadDelta.state) {
         let state = downloadDelta.state.current;
         if (state === "complete") {
             let id = downloadDelta.id;
-            let hash = computeHash(downloads[id].filename);
-            downloads[id] = {
-                hash: hash
+            downloads[id].state = "complete";
+            if (downloads[id].checksums === undefined || downloads[id].algorithms === undefined) {
+                console.log("Cannot verify the hash value!")
+            } else {
+                let computedHashes = await computeHashes(downloads[id].filename, getHashesFromNames(downloads[id].tab.algorithms));
+                downloads[id].computedHashes = computedHashes;
+                if (downloads[id].tab.checksums.some(value => computedHashes.includes(value))) {
+                    console.log("The checksum is valid!");
+                } else {
+                    console.log("The checksum is NOT valid!");
+                }
             }
         } else if (state === "interrupted") {
             delete downloads[downloadDelta.id];
@@ -54,44 +67,151 @@ chrome.downloads.onChanged.addListener((downloadDelta) => {
 })
 
 /**
+ * Wrapper function around getHashFromName() that supplies hash objects out of multiple names
+ * @param names
+ * @returns {Set<any>}
+ */
+function getHashesFromNames(names) {
+    let set = new Set();
+    names.forEach((elem) => {
+        set.add(getHashFromName(elem));
+    })
+    return set;
+}
+
+/**
+ * Get a hash object from crypto library based on a string representation
+ * @param name
+ * @returns {Hasher}
+ */
+function getHashFromName(name) {
+    let algorithms = cryptoJs.algo;
+    switch (name) {
+        case "sha1":
+            return algorithms.SHA1.create();
+        case "sha224":
+            return algorithms.SHA224.create();
+        case "sha256":
+            return algorithms.SHA256.create();
+        case "sha384":
+            return algorithms.SHA384.create();
+        case "sha512":
+            return algorithms.SHA512.create();
+        case "md5":
+            return algorithms.MD5.create();
+        case "sha3224":
+            return algorithms.SHA3.create({outputLength: 224});
+        case "sha3256":
+            return algorithms.SHA3.create({outputLength: 256});
+        case "sha3384":
+            return algorithms.SHA3.create({outputLength: 384});
+        case "sha3512":
+            return algorithms.SHA3.create({outputLength: 512});
+    }
+}
+
+/**
  * Fetch the file given filename, and compute a checksum from the acquired stream.
  * @param filename
- * @returns Hash of the file
+ * @param hashes array of hash objects
+ * @returns array of the file hashes, with hash object as a key
  */
-function computeHash(filename) {
-    console.debug("Inside computeHash");
-    let hashValue;
-    fetch(filename)
-        .then(response => {
-            if (response.ok) {
-                const reader = response.body.getReader();
-                let totalByteLen = 0;
-                const read = () => {
-                    reader.read().then(({done, value}) => {
-                        if (done) {
-                            let start = performance.now();
-                            hashValue = hash.finalize().toString();
-                            let total = (performance.now() - start) / 1000;
-                            console.log("Hash for file: " + filename + " took "
-                                + total + "seconds to compute. The value is:\n" + hashValue);
-                            console.debug("Total size (bytes): " + totalByteLen);
-                            reader.releaseLock();
-                        } else {
-                            console.debug("New chunk length: " + value.byteLength);
-                            totalByteLen += value.byteLength;
-                            hash.update(crypto.lib.WordArray.create(value));
-                            read();
-                        }
-                    }).catch(reason => {
-                        console.debug(reason);
-                    });
+async function computeHashes(filename, hashes) {
+    console.debug("Inside computeHashes");
+    let hashValues = [];
+    try {
+        fetch(filename)
+            .then(response => {
+                if (response.ok) {
+                    const reader = response.body.getReader();
+                    let totalByteLen = 0;
+                    const read = () => {
+                        reader.read().then(({done, value}) => {
+                            if (done) {
+                                for (let hash in hashes) {
+                                    hashValues[hash] = hash.finalize().toString().toLowerCase();
+                                }
+                                console.log("Hash for file: " + filename + " is:\n" + hashValues.toString());
+                                console.debug("Total size (bytes): " + totalByteLen);
+                                reader.releaseLock();
+                            } else {
+                                console.debug("New chunk of length: " + value.byteLength);
+                                totalByteLen += value.byteLength;
+                                for (let hash in hashes) {
+                                    hash.update(cryptoJs.lib.WordArray.create(value));
+                                }
+                                read();
+                            }
+                        }).catch(reason => {
+                            console.debug(reason);
+                        });
+                    }
+                    read();
+                } else {
+                    console.debug("Response status: " + response.status);
                 }
-                read();
-            } else {
-                console.debug("Response status: " + response.status);
-            }
-        }).catch((reason) => {
-        console.debug(reason);
-    });
-    return hashValue;
+            })
+            .catch((reason) => {
+                console.debug(reason);
+            });
+    } catch (e) {
+        console.error("Failed to fetch a file. " + e)
+    }
+    return hashValues;
 }
+
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    switch (message.type) {
+        case "content":
+            tabsData.unshift({
+                urls: message.urls,
+                checksums: message.checksums,
+                algorithms: message.algorithms
+            });
+            break;
+        case "noContent":
+            tabsData.unshift({
+                urls: message.urls
+            })
+            break;
+        case "keepAlive":
+            break;
+    }
+    // https://github.com/mozilla/webextension-polyfill/issues/130#issuecomment-531531890
+    sendResponse({});
+    return true;
+});
+
+/**
+ * Verify if URL file access is allowed. If not display instructions in a new tab.
+ */
+chrome.extension.isAllowedFileSchemeAccess(function (isAllowed) {
+    if (!isAllowed) {
+        chrome.tabs.create({url: "settings/" + chrome.i18n.getMessage("lang") + "/instructions.html"})
+    }
+});
+
+/**
+ * Schedule next alarm
+ */
+function keepAlive() {
+    console.debug("Background request to keep alive");
+    chrome.alarms.create("keepAlive", {when: Date.now() + 1000});
+}
+
+/**
+ * Keep background script alive if there is at least one ongoing download
+ */
+chrome.alarms.onAlarm.addListener((alarm) => {
+    switch (alarm.name) {
+        case "keepAlive":
+            for (let i=0; i < downloads.size; i++) {
+                if (downloads[i].state !== "complete") {
+                    keepAlive();
+                    break;
+                }
+            }
+            break;
+    }
+});
--- src/contentScript.js
+++ src/contentScript.js
@@ -1,44 +1,52 @@
-'use strict';
+"use strict";
+
 const CHECKSUM_VALUE_REGEX = /[a-f\d]{32,128}|[A-F\d]{32,128}/g;
 const CHECKSUM_SIZE_ARRAY = [32, 40, 56, 64, 96, 128];
-const CHECKSUM_ALG_REGEX = /(sha)-?(1|224|256|2|384|3|512|5)|(md)-?5/gi;
+const CHECKSUM_ALG_REGEX = /(sha)-?(1|224|256|384|512)|(md)-?5|(sha3-)(224|256|384|512)/gi;
 const DANGEROUS_EXTENSIONS = ["dmg", "exe", "msi", "pkg", "iso", "zip", "tar.xz"
     , "tar.gz", "tar.bz2", "tar", "deb", "rpm", "appimage"];
 
+//TODO variable should be set from within settings/options page
+// chrome.storage.sync.set({onlyMatchDangerous: false}, () => {
+//     console.log("value set");
+// });
+
 let checksums = [];
 let algorithms = [];
+let urls = [];
 
 function extractPattern(pattern) {
     let set = new Set();
 
     // Gather all HTML nodes; If a node contains a pattern, clean up and add all matching values to the set
     document.querySelectorAll('*').forEach((value) => {
-        if (pattern.test(value.innerHTML)){
+        if (pattern.test(value.innerHTML)) {
             value.innerHTML.match(pattern).forEach((element) => {
-                set.add(element.toLowerCase().replace('-',''));
+                set.add(element.toLowerCase().replace('-', ''));
             });
         }
     });
 
     // Print all matching values on debug level
-    console.debug("Matches for pattern: \"" + pattern + "\"");
-    let iterator = set.values();
-    let result = iterator.next();
-    while (!result.done) {
-        console.debug(result.value);
-        result = iterator.next();
-    }
+    console.debug(set.size + " matches for pattern: \"" + pattern + "\"");
+    // let iterator = set.values();
+    // let result = iterator.next();
+    // while (!result.done) {
+    //     console.debug(result.value);
+    //     result = iterator.next();
+    // }
     return set;
 }
 
 /**
- * Filter hash values based on hasMix() result and validity of length
+ * Filter hash values based on hasMix() result and validity of length.
  * @param values
  * @returns {Set<any>}
  */
 function filterHashes(values) {
     /**
-     * Checks whether a given hash value contains both digits and letters, and if the variety of characters is high enough.
+     * Checks whether a given hash value contains both digits and letters, and
+     * if the variety of characters is high enough.
      * @param value
      * @returns {boolean}
      */
@@ -63,37 +71,76 @@ function filterHashes(values) {
 }
 
 /**
- * Top level function extracting and filtering hash values and algorithm names
+ * Return true if filename matches any of the dangerous file extensions.
+ * @param filename
+ * @returns {boolean}
  */
-function extractData() {
-    checksums = filterHashes(extractPattern(CHECKSUM_VALUE_REGEX));
+function isFileExtensionDangerous(filename) {
+    DANGEROUS_EXTENSIONS.forEach((value) => {
+        if (filename.toLowerCase().endsWith(value.toLowerCase())) {
+            return true;
+        }
+    });
+    return false;
+}
 
+/**
+ * Extract download links from all "a" elements present in DOM. This is a
+ * workaround to overcome limitations in chrome.downloads API which does not
+ * provide data about tab from which the download has been initiated.
+ * @returns {Set<any>}
+ */
+function extractLinks() {
+    let urls = new Set();
+    let extractAll = true;
+    // chrome.storage.sync.get(onlyMatchDangerous, ({onlyMatchDangerous}) => {
+    //     console.log("value received: " + onlyMatchDangerous);
+    //     extractAll = onlyMatchDangerous;
+    // });
+    document.querySelectorAll("a").forEach((element) => {
+        if (element.hasAttribute("href")) {
+            let link = element.href;
+            if (!extractAll) {
+                if (isFileExtensionDangerous(link)) {
+                    urls.add(link);
+                }
+            } else {
+                urls.add(link);
+            }
+        }
+    });
+    return urls;
+}
+
+/**
+ * Top level function that extracts, filters and sends hash values along with algorithm names and links to the background script
+ */
+async function extractData() {
+    checksums = filterHashes(extractPattern(CHECKSUM_VALUE_REGEX));
     algorithms = extractPattern(CHECKSUM_ALG_REGEX);
+    urls = extractLinks();
+    let msgFailed = () => {
+        console.debug("Failed to send message")
+    }
+    if (checksums.size !== 0 && algorithms.size !== 0) {
+        chrome.runtime.sendMessage({
+            type: "content",
+            checksums: [...checksums],
+            algorithms: [...algorithms],
+            urls: [...urls]
+        }).catch(() => msgFailed());
+    } else {
+        console.log("No checksum/algorithm pairs found!")
+        chrome.runtime.sendMessage({
+            type: "noContent",
+            urls: [...urls]
+        }).catch(() => msgFailed());
+    }
+    for (let i = 0; i < 120; i++) {
+        const sleep = ms => new Promise(res => setTimeout(res, ms));
+        await sleep(1000);
+        chrome.runtime.sendMessage({type: "keepAlive"}).catch(() => msgFailed())
+    }
 }
 
 extractData();
-
-// Communicate with background file by sending a message
-// chrome.runtime.sendMessage(
-//   {
-//     type: 'GREETINGS',
-//     payload: {
-//       message: 'Hello, my name is Con. I am from ContentScript.',
-//     },
-//   },
-//   response => {
-//     console.log(response.message);
-//   }
-// );
-
-// Listen for message
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//   if (request.type === 'COUNT') {
-//     console.log(`Current count is ${request.payload.count}`);
-//   }
-//
-//   // Send an empty response
-//   // See https://github.com/mozilla/webextension-polyfill/issues/130#issuecomment-531531890
-//   sendResponse({});
-//   return true;
-// });
